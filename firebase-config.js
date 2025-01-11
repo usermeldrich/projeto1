08f71d03e43cfc209d5593469ed9681b33a25bb8
usermeldrich <tomaxhereandnow@gmail.com>
2025-01-11 15:33:52 -0300

    ATT
    novas funções

---
+++ firebase-config.js
@@ -0,0 +1,16 @@
+// Configuração do Firebase
+const firebaseConfig = {
+    apiKey: "AIzaSyA12MtXQgpFORG4bqQkIqDcYmcgN66uFcU",
+    authDomain: "projeto1-98db0.firebaseapp.com",
+    projectId: "projeto1-98db0",
+    storageBucket: "projeto1-98db0.firebasestorage.app",
+    messagingSenderId: "1040325681203",
+    appId: "1:1040325681203:web:e5dc92db312fdfa6860346",
+    measurementId: "G-PN1BCB2NX9"
+  };
+  
+  // Inicialização do Firebase
+  const app = firebase.initializeApp(firebaseConfig);
+  const auth = firebase.auth(); // Para autenticação
+  export { app, auth };
+  
