5f001af419e19fef0d46beeca8fc761cea637fd2
usermeldrich <tomaxhereandnow@gmail.com>
2025-01-14 20:15:50 -0300

    teste

--- firebase-config.js
+++ firebase-config.js
@@ -1,3 +1,9 @@
+// Importação dos módulos necessários do Firebase (Firebase v9+)
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-firestore.js";
+import { getDatabase } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-database.js";
+
 // Configuração do Firebase
 const firebaseConfig = {
     apiKey: "AIzaSyA12MtXQgpFORG4bqQkIqDcYmcgN66uFcU",
@@ -7,12 +13,12 @@ const firebaseConfig = {
     messagingSenderId: "1040325681203",
     appId: "1:1040325681203:web:e5dc92db312fdfa6860346",
     measurementId: "G-PN1BCB2NX9"
-  };
-  
-  // Inicialização do Firebase
-  
-  const app = firebase.initializeApp(firebaseConfig);
-  const auth = firebase.auth(); // Para autenticação
-  const db = firebase.firestore();
-  export { app, auth };
-  
+};
+
+// Inicialização do Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);  // Para autenticação
+const db = getFirestore(app);  // Para Firestore
+const rtdb = getDatabase(app);  // Para Realtime Database
+
+export { app, auth, db, rtdb };
